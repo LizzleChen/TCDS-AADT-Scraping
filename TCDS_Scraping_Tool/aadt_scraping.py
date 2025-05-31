ebff5ed6f567873a1adcd3e4ed93a7bdf7af7155
lchen <lchen@candm-associates.com>
2025-05-31 17:47:10 -0500

    Get data by directions and output to json-like format

--- TCDS_Scraping_Tool/aadt_scraping.py
+++ TCDS_Scraping_Tool/aadt_scraping.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException, ElementClickInterceptedException
+from selenium.common.exceptions import TimeoutException, ElementClickInterceptedException, NoSuchElementException
 import time
 import random
 import re
@@ -22,8 +22,8 @@ def open_tcds_detail_page(id):
     """
     global driver
     options = webdriver.ChromeOptions()
-    options.browser_version = 'stable'
-    options.capabilities['browserVersion'] = 'stable'
+    # options.browser_version = 'stable'
+    # options.capabilities['browserVersion'] = 'stable'
     driver = webdriver.Chrome()
     driver.get(
         f'https://txdot.public.ms2soft.com/tcds/set_session.asp?ext=y&loc=txdot&LOCAL_ID={id}'
@@ -40,7 +40,7 @@ def check_dir():
     If there are different directions, after finishing the two-way, scrape each direction. 
 
     Returns: 
-        A list of available directions ["NB", "SB"] or [] for "two-way" only
+        A list of available directions ["NB", "SB"] or None for "two-way" only
     """
 
     # One-way or Two-way X-Path: //*[@id="DIR_BUTTONS_DIV"]/span/div[2]/input
@@ -49,6 +49,28 @@ def check_dir():
     values = [element.get_attribute('value') for element in input_elements if element.get_attribute('value') in ["NB", "SB", "EB", "WB"]]
     return values
 
+def click_dir_button(dir: str, timeout = 20):
+    global driver
+    xpath = f"//div[@id='DIR_BUTTONS_DIV']//input[@value='{dir}']"
+    print(f"Getting Direction {dir} direction")
+    try:
+        element = WebDriverWait(driver, timeout).until(EC.element_to_be_clickable((By.XPATH, xpath)))
+        element.click()
+
+        time.sleep(2)
+
+        # Check if that direction button is click by checking onClick is "javascript:void(0)" - if so, exit
+        element = WebDriverWait(driver, timeout).until(EC.presence_of_element_located((By.XPATH, xpath))) #Webpage will refresh, so we will re-locate the element
+        onclick_value = element.get_attribute('onclick')
+        if onclick_value == "javascript:void(0)":
+            print(f"'{dir}' has been clicked")
+            return True
+    
+    except NoSuchElementException:
+        print(f"Element with direction value of '{dir}' not found")
+        return False
+    
+    return False
 
 
 def scrape_aadt_data(tablediv_xpath = ".//tr[@class='FormRowLabel']/following-sibling::tr", timeout=20):
@@ -60,6 +82,9 @@ def scrape_aadt_data(tablediv_xpath = ".//tr[@class='FormRowLabel']/following-si
     
     global driver
     all_aadt = []
+
+    #Create check set to check for repetitive years. Sometimes the website is slow responding to next page button click, and we will get duplicated years in our outputs. 
+    seen_year = set()
     
     while True:
         try:
@@ -71,8 +96,7 @@ def scrape_aadt_data(tablediv_xpath = ".//tr[@class='FormRowLabel']/following-si
             #have to wait a few seconds before selenium be able to use the same element after page changes.
             time.sleep(random.randrange(5,6)) 
 
-            #Create check set to check for repetitive years. Sometimes the website is slow responding to next page button click, and we will get duplicated years in our outputs. 
-            seen_year = set()
+
 
             # Get the data from current page
             rows = table_div.find_elements(By.XPATH, tablediv_xpath)
@@ -153,9 +177,18 @@ def process_single_id(id: str) -> None:
     global driver
     print(f"Processing ID: {id}")
     open_tcds_detail_page(id)
-    check_dir()
     aadt = scrape_aadt_data()
     append_to_json(id, "output.json", aadt)
+    directions = check_dir()
+    if directions:
+        for dir in directions:
+            #click direction function
+            if click_dir_button(dir):
+                aadt = scrape_aadt_data()
+                append_to_json(id+"_"+dir, "output.json", aadt)
+            else:
+                print(f"Failed to retrieve Station {id} directional information")
+
     # export_to_csv(id, aadt)
     driver.quit()
 
